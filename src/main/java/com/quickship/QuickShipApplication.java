9cd5b2ede8ec00d6e05e772b71bcb8f328a17b2f
Lokesh <lokeshgundapaneni@gmail.com>
2026-08-04 18:48:25 +0530

    initial springboot project setup

---
+++ src/main/java/com/quickship/QuickShipApplication.java
@@ -0,0 +1,13 @@
+package com.quickship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickShipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickship/QuickShipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickShipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
