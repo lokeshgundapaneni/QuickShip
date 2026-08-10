f760f3639b468fab3a5501f08c1c21b9e6487ad5
Lokesh <lokeshgundapaneni@gmail.com>
2026-08-10 17:06:04 +0530

    feat(shipment):add shipment repository

---
+++ src/main/java/com/quickship/shipment/repository/ShipmentRepository.java
@@ -0,0 +1,7 @@
+package com.quickship.shipment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShipmentRepository extends JpaRepository<T, ID> {
+
+}
